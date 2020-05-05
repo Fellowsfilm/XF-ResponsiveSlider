dc3e938802da82d2566bcd092fbf336bad94c436
Rylea Stark <rylea@versobit.com>
2020-05-05 05:44:27 -0600

    Implement Widget Install and Template
    - Widget Template has been injected
    - Widget is instructed to be created on install
    - Added toggle option for enabling or disabling the slider.
    
    Fixes #5

--- VersoBit/ResponsiveSlider/Setup.php
+++ VersoBit/ResponsiveSlider/Setup.php
@@ -6,18 +6,23 @@ use XF\AddOn\AbstractSetup;
 
 class Setup extends AbstractSetup
 {
-	public function install(array $stepParams = [])
+	public function install()
 	{
-		// TODO: Implement install() method.
+		$this->createWidget('vbResponsiveSlider', 'html', [
+		'positions' => ['forum_overview_top' => 1],
+		'options' => ['advanced_mode' => true,
+				'template_title' => '[VersoBit] Responsive Slider',
+			 ]
+		 ]);
 	}
 
-	public function upgrade(array $stepParams = [])
+	public function upgrade()
 	{
 		// TODO: Implement upgrade() method.
 	}
 
-	public function uninstall(array $stepParams = [])
+	public function uninstall()
 	{
-		// TODO: Implement uninstall() method.
+		// TODO: Implement uninstall() method. 
 	}
 }
